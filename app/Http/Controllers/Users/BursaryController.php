ecb066c59e818df76a4ca1570fb6d5cfcc773d58
teewhy <hamendment@gmail.com>
2021-04-27 11:58:00 +0100

    Update 27-04-2021

--- app/Http/Controllers/Users/BursaryController.php
+++ app/Http/Controllers/Users/BursaryController.php
@@ -5,11 +5,14 @@ namespace App\Http\Controllers\Users;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Payment;
+use App\Models\Transaction;
 use App\Exports\PaymentExport;
 use App\Imports\PaymentImport;
 use App\Imports\ResultImport;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use Maatwebsite\Excel\Facades\Excel;
+use App\Http\Controllers\Users\AdmissionOfficer;
 
 class BursaryController extends Controller
 {
@@ -81,8 +84,72 @@ class BursaryController extends Controller
     }
 
 
-    public function all_application_fee_paid(Request $request)
+    public function all_applicant_fee_paid(Request $request)
     {
-        
+        $semester = null;
+        $session = null;
+        $transaction = null;
+        if ($request->semester == null)
+        {$semester = AdmissionOfficer::settings($request)->semester_name;}else{$semester=$request->semester;}
+        if ($request->session == null)
+        {$session = AdmissionOfficer::settings($request)->session_name;}else{$session=$request->session;}
+        if($request->has('applicationFee') && $request->filled('applicationFee')){
+            $transaction = $this->sql_all_applicant_fee_paid('APPLICATION',$semester,$session,$degree=$request->degree);
+        }
+        else if($request->has('acceptanceFee') && $request->filled('acceptanceFee')){
+           
+            $transaction = $this->sql_all_applicant_fee_paid('ACCEPTANCE',$semester,$session,$degree=$request->degree);
+        }
+        else if($request->has('mcFee') && $request->filled('mcFee')){ 
+            $transaction = $this->sql_all_applicant_fee_paid('CAUTION',$semester,$session,$degree=$request->degree);
+
+        }
+        else{
+            $transaction = $this->sql_all_applicant_fee_paid(null,$semester,$session,$degree=$request->degree);
+
+        }
+        return response()->json(['msg' => 'success', 'transaction' => $transaction]);
+
+
+    }
+
+
+    
+
+    public function sql_all_applicant_fee_paid($param1,$semester,$session,$degree)
+    {
+        try {
+
+            if($param1 == null){
+                return  DB::table('transactions')
+                ->select('transactions.rrr','payments.type','transactions.amount','transactions.status','payments.programme_type',
+                'applicants.email','applicants.mobile','applicants.surname','applicants.lastname')
+                ->join('applicants','transactions.transaction_id','applicants.id')
+                ->join('payments','transactions.payment_id','payments.id')
+                ->where('transactions.semester_name',$semester)
+                ->where('transactions.session_name',$session)
+                ->whereNotNull('transactions.rrr') 
+                ->when($degree, function($query) use($degree){
+                    $query->where('payments.programme_type', $degree);
+                })
+                ->orderBy('transactions.created_at')->get();
+            }
+    
+            
+          return  DB::table('transactions')
+            ->select('transactions.rrr','payments.type','transactions.amount','transactions.status','payments.programme_type',
+            'applicants.email','applicants.mobile','applicants.surname','applicants.lastname')
+            ->join('applicants','transactions.transaction_id','applicants.id')
+            ->join('payments','transactions.payment_id','payments.id')
+            ->where('payments.type', $param1)->where('transactions.semester_name',$semester)
+            ->where('transactions.session_name',$session)->whereNotNull('transactions.rrr')
+            ->orderBy('transactions.created_at')
+            ->when($degree, function($query) use($degree){
+                $query->where('payments.programme_type', $degree);
+            })
+            ->orderBy('transactions.created_at')->get();
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
     }
 }
